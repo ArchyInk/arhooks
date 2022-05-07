5833ab27f01320e799fa3d9691f71aef94f43085
archy <archybool@gmail.com>
2022-05-07 11:46:00 +0800

    commit错目录了

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+const path = require('path')
+
+export default defineConfig(({ mode }) => {
+  if (mode === 'vue') {
+    return {
+      root: './vue',
+      plugins: [vue(), vueJsx()]
+    }
+  } else if (mode === 'react') {
+    return {
+      root: './react',
+      plugins: [react()]
+    }
+  }
+})
+
---
+++ vue/components/test.tsx
@@ -0,0 +1,17 @@
+/*
+ * @author: Archy
+ * @Date: 2022-03-22 11:52:25
+ * @LastEditors: Archy
+ * @LastEditTime: 2022-03-22 11:52:25
+ * @FilePath: \vite-ts-vue\src\components\test.tsx
+ * @description: 
+ */
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: '',
+  setup() {
+    return () => {
+      return
+    }
+  }
+})
